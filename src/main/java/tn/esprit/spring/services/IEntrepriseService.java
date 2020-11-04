72c0060284e2077b44b8eb68b7199d45f24e7693
bmahjoubi <bmahjoubi1@gmail.com>
2020-11-04 15:01:23 +0100

    correction des bugs

--- src/main/java/tn/esprit/spring/services/IEntrepriseService.java
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -11,7 +11,7 @@ public interface IEntrepriseService {
 	public int ajouterDepartement(Departement dep);
 	void affecterDepartementAEntreprise(int depId, int entrepriseId);
 	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);
-	public void deleteEntrepriseById(int entrepriseId);
-	public void deleteDepartementById(int depId);
+	public Boolean deleteEntrepriseById(int entrepriseId);
+	public Boolean deleteDepartementById(int depId);
 	public Entreprise getEntrepriseById(int entrepriseId);
 }
